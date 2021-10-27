0e56a90b63c100165f242502a4ad3cf9495d6165
daniml3 <danimoral1001@gmail.com>
2021-10-27 17:07:06 +0000

    sdm660-common: drop QTI datastatusnotification [2/2]
    Change-Id: I63e245e3ced484a95f7075ddbd64d07e074e09db

--- sdm660-common/sdm660-common-vendor.mk
+++ sdm660-common/sdm660-common-vendor.mk
@@ -524,7 +524,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
