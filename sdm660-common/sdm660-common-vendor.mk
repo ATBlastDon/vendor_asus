dafb0978188530b0876a1bfd05450aec829b5bf9
AT WORLD <atharvsutar3102003@gmail.com>
2023-12-15 10:13:13 +0530

    Revert "sdm660-common: Import datastatusnotification from coral [2/2]"
    This reverts commit 6cfa774676a3c00f746f5558afc895945da66f9f.

--- sdm660-common/sdm660-common-vendor.mk
+++ sdm660-common/sdm660-common-vendor.mk
@@ -640,7 +640,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
